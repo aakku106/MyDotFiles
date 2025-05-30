b7cc50d7ae790651bbe8b0b9df9b3ade8780d172
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-05-30 15:34:41 +0545

    Created a Install.sh
    why do menual lever when i learned we can do something this simple XD

---
+++ Install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -sfn ~/.dotfiles/.zshrc ~/.zshrc
+ln -sfn ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -sfn ~/.dotfiles/.config ~/.config
+ln -sfn ~/.dotfiles/.tmux ~/.tmux
+ln -sfn ~/.dotfiles/.tmux.conf ~/.tmux.conf
+
+
