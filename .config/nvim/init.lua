7d3bd24b7202de8e74b92c37abba00ef8be3f1fb
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-06-26 23:25:19 +0545

    Added tailwind,js and html/css server for better support in react

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -669,29 +669,22 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
+        clangd = {}, -- already had this (C/C++)
+        -- gopls = {},            -- optional (Go)
+        -- pyright = {},         -- optional (Python)
+        -- rust_analyzer = {},   -- optional (Rust)
+
         ts_ls = {},
-        --
+        tailwindcss = {}, -- 🆕 for Tailwind class autocomplete
+        html = {}, -- 🆕 for HTML tag support
+        cssls = {}, -- 🆕 for regular CSS support
 
         lua_ls = {
-          -- cmd = { ... },
-          -- filetypes = { ... },
-          -- capabilities = {},
           settings = {
             Lua = {
               completion = {
                 callSnippet = 'Replace',
               },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
               -- diagnostics = { disable = { 'missing-fields' } },
             },
           },
