bde6d16ca37b1982625973dbbdddf35d10bdc821
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-07-10 10:44:34 +0545

    Clear comment and added alias for exit

--- zsh/aliases/geneeral.zsh
+++ zsh/aliases/geneeral.zsh
@@ -1,6 +1,7 @@
 
 alias cd="z"
 alias c="clear"
+alias e='exit'
 alias nv="nvim"
 alias la="lazygit"
 alias wee="caffeinate -d"
@@ -9,5 +10,4 @@ alias b="zsh"
 alias y='yazi'
 alias l="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
 alias ls="clear && eza --color=always --long --git  --icons=always -a"
-# Naruto mode with anime background + orange theme
 alias naruto-mode="kitty --config ~/.config/kitty/naruto.conf"
