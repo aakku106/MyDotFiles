018fb7107a0f8844c74cca5cdb3353058f2d7127
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-07-10 10:41:52 +0545

    Added yazi

--- zsh/aliases/geneeral.zsh
+++ zsh/aliases/geneeral.zsh
@@ -6,6 +6,7 @@ alias la="lazygit"
 alias wee="caffeinate -d"
 alias weee="caffeinate -d"
 alias b="zsh"
+alias y='yazi'
 alias l="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
 alias ls="clear && eza --color=always --long --git  --icons=always -a"
 # Naruto mode with anime background + orange theme
