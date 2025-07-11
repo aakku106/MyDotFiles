7aee24559f664ca2e37f1a3a5d06eadc83c973fb
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-07-11 17:19:20 +0545

    Added a custom script for yazi to cd into the folder we pressed q[quit]

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,3 +1,4 @@
+# Plugin setup
 eval "$(fzf --zsh)"
 eval $(thefuck --alias)
 eval $(thefuck --alias fk)
@@ -5,3 +6,14 @@ eval "$(zoxide init zsh)"
 
 source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# Custom Yazi wrapper (no auto-launch, works perfect)
+yazi() {
+  local tmpfile=$(mktemp)
+  command yazi --cwd-file="$tmpfile" "$@"
+  if [[ -f "$tmpfile" ]]; then
+    local dir=$(<"$tmpfile")
+    rm -f "$tmpfile"
+    [[ -d "$dir" ]] && cd "$dir"
+  fi
+}
