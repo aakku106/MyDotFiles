87a0ec9eb427b9bcf75f26056c3d7134dd3b7a15
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-10-18 00:41:42 +0545

    added bb for reloed insted of using b fro zsh every time

--- zsh/aliases/geneeral.zsh
+++ zsh/aliases/geneeral.zsh
@@ -1,4 +1,3 @@
-
 alias cd="z"
 alias c="clear"
 alias e='exit'
@@ -7,6 +6,7 @@ alias la="lazygit"
 alias wee="caffeinate -d"
 alias weee="caffeinate -d"
 alias b="zsh"
+alias bb="source ~/.zshrc"
 alias y='yazi'
 alias l="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
 alias ls="clear && eza --color=always --long --git  --icons=always -a"
