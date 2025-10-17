b7dd68717c2833839e4b4a05483954dc800082fc
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-10-18 00:54:23 +0545

    added clear after bb runs

--- zsh/aliases/geneeral.zsh
+++ zsh/aliases/geneeral.zsh
@@ -6,7 +6,7 @@ alias la="lazygit"
 alias wee="caffeinate -d"
 alias weee="caffeinate -d"
 alias b="zsh"
-alias bb="source ~/.zshrc"
+alias bb="source ~/.zshrc && clear"
 alias y='yazi'
 alias l="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
 alias ls="clear && eza --color=always --long --git  --icons=always -a"
