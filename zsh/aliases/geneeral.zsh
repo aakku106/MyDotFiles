5b1500eed9296806ee3df801856b0e1f11d6aa0a
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-11-22 19:13:58 +0545

    added bat support for l or ls through eza from , command, but without header

--- zsh/aliases/geneeral.zsh
+++ zsh/aliases/geneeral.zsh
@@ -7,6 +7,6 @@ alias wee="caffeinate -d"
 alias weee="caffeinate -d"
 alias b="c && source ~/.zshrc"
 alias y='yazi'
-alias l="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions | bat "
+alias l="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions | bat --style=-header --paging=never --color=always"
 alias ls="clear && eza --color=always --long --git  --icons=always -a"
 alias naruto="kitty --config ~/.config/kitty/naruto.conf"
