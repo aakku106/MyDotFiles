df92563f788e8087608e5c17bc44675c25233d50
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-11-22 20:43:08 +0545

    added a crazy logic in cd
    just try it u will find it what it is just type
    cd and press enter
    or write cd a give arg as usual

--- zsh/aliases/geneeral.zsh
+++ zsh/aliases/geneeral.zsh
@@ -1,4 +1,3 @@
-alias cd="z"
 alias l="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions | bat --style=-header --paging=never"
 alias ls="clear && eza --color=always --long --git  --icons=always -a | bat --style=-header --paging=never"
 alias c="clear"
@@ -12,9 +11,19 @@ alias weee="caffeinate -d"
 # Smart nvim: uses fzf when no args, otherwise passes args to nvim
 nv() {
   if [ $# -eq 0 ]; then
-    nvim $(eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions | fzf --ansi | awk '{print $NF}')
+    nvim $(eza -a --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions | fzf --ansi | awk '{print $NF}')
   else
     nvim "$@"
+  fi
+}
+
+# Smart CD: uses fzf when no args, otherwise passes args to cd
+cd() {
+  if [ $# -eq 0 ]; then
+    local dir=$( (echo ".. 󰜱 " && eza -D -a --color=always  | sed 's/^/  /') | fzf --ansi --height=40% | awk '{print $1}')
+    [ -n "$dir" ] && z "$dir"
+  else
+    z "$@"
   fi
 }
 
