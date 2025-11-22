e9eec4a472ee54571c27839da895fa01ba505f10
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-11-22 19:34:52 +0545

    added a fun logic in nv
    if we didnt gave any args than it will opean fzf with eza
    and if we gave args it will work as normal

--- zsh/aliases/geneeral.zsh
+++ zsh/aliases/geneeral.zsh
@@ -1,12 +1,21 @@
 alias cd="z"
-alias c="clear"
-alias e='exit'
-alias nv="nvim"
-alias la="lazygit"
-alias wee="caffeinate -d"
-alias weee="caffeinate -d"
-alias b="c && source ~/.zshrc"
-alias y='yazi'
 alias l="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions | bat --style=-header --paging=never"
 alias ls="clear && eza --color=always --long --git  --icons=always -a | bat --style=-header --paging=never"
+alias c="clear"
+alias e='exit'
+alias b="c && source ~/.zshrc"
+alias la="lazygit"
+alias y='yazi'
+alias wee="caffeinate -d"
+alias weee="caffeinate -d"
+
+# Smart nvim: uses fzf when no args, otherwise passes args to nvim
+nv() {
+  if [ $# -eq 0 ]; then
+    nvim $(eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions | fzf --ansi | awk '{print $NF}')
+  else
+    nvim "$@"
+  fi
+}
+
 alias naruto="kitty --config ~/.config/kitty/naruto.conf"
