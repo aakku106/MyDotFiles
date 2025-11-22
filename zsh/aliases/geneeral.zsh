08b84c599a379b42c3fe15a0850fb22625d5a0e6
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2025-11-22 19:15:55 +0545

    also adding bat in ls command its just verbost version of l

--- zsh/aliases/geneeral.zsh
+++ zsh/aliases/geneeral.zsh
@@ -8,5 +8,5 @@ alias weee="caffeinate -d"
 alias b="c && source ~/.zshrc"
 alias y='yazi'
 alias l="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions | bat --style=-header --paging=never --color=always"
-alias ls="clear && eza --color=always --long --git  --icons=always -a"
+alias ls="clear && eza --color=always --long --git  --icons=always -a | bat --style=-header --paging=never"
 alias naruto="kitty --config ~/.config/kitty/naruto.conf"
