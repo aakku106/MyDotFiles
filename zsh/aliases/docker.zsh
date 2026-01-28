fe592c1a02611b4ec189c78c741a897835115a08
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-01-29 00:40:09 +0545

    fallback -will be changed
    - this is temp fall back i will modify it latter on

--- zsh/aliases/docker.zsh
+++ zsh/aliases/docker.zsh
@@ -3,6 +3,8 @@ alias dk='docker'
 # c: container
 alias dkcl="dk container ps"
 alias dkcla="dk container ps -a"
+alias dkc="dkcl"
 # i: image
 alias dkil="dk image ls"
 alias dkila="dk image ls -a"
+alias dki="dkil"
