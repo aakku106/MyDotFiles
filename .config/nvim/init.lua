c72fe7ae0497c90a6633e0f8c2f09ba75692ad14
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-03-07 15:56:00 +0545

    consolidated git configuration in gitsigns with signs and keymaps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -268,19 +268,8 @@ require('lazy').setup({
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`.
   --
-  -- See `:help gitsigns` to understand what the configuration keys do
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
+  -- NOTE: Git integration (gitsigns) is configured in kickstart/plugins/gitsigns.lua
+  -- See that file for signs configuration and all git-related keymaps
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
